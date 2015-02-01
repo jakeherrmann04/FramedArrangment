d4df561aff623f0547d12b9c357562ff0b5560e9
jakeherrmann04 <jake.herrmann04@gmail.com>
2015-01-31 21:12:11 -0700

    First Commit

---
+++ FramedArrangment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FramedArrangment
+//
+//  Created by Jake Herrmann on 1/25/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FramedArrangment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FramedArrangment
+//
+//  Created by Jake Herrmann on 1/25/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
